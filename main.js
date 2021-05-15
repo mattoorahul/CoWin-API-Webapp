32725cf324d7f192a3e673124726531af5a0a003
Vinod Mattoo <rahulmattoo0@gmail.com>
2021-05-15 19:10:34 +0530

    cowin webapp v1.0

---
+++ main.js
@@ -0,0 +1,438 @@
+
+
+var tempData = {
+    "centers": [
+        {
+            "center_id": 8798,
+            "name": "ESI Maya Puri Phase II PHC",
+            "address": "22 Facility Area Near Panjab Sindh Bank Mayapuri Phase 2",
+            "state_name": "Delhi",
+            "district_name": "New Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 28,
+            "long": 77,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "69534a3d-63fa-45b4-90af-a1e1c1ebaad9",
+                    "date": "22-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVAXIN",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "34ae9632-44cf-44bb-a764-a8820c83ba9e",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVAXIN",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "100b91fe-0a7f-46bf-930e-ff8dca850956",
+                    "date": "25-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVAXIN",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "e5a2b234-d609-4825-89c2-25b698ddbb2e",
+                    "date": "27-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVAXIN",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                }
+            ]
+        },
+        {
+            "center_id": 682529,
+            "name": "MCW MAHENDRA PARK",
+            "address": "MAHENDRA PARK UTTAM NAGAR",
+            "state_name": "Delhi",
+            "district_name": "South West Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 0,
+            "long": 0,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "e75d5c6a-b2d4-4e62-8679-4637c6f3adfa",
+                    "date": "22-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "e8b17925-2c1b-462b-b292-1864f58585be",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "bd5a9657-13ff-4189-9194-bb0203a7f78b",
+                    "date": "25-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "022e0288-63b7-405c-b468-407682ddefcc",
+                    "date": "27-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                }
+            ]
+        },
+        {
+            "center_id": 682530,
+            "name": "MCW JANTA FLATS",
+            "address": "Mohan Garden Nawada Extension",
+            "state_name": "Delhi",
+            "district_name": "South West Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 0,
+            "long": 0,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "b6061fdd-d67f-4998-84c3-8c5b532b71d2",
+                    "date": "22-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "69b688e4-47e4-43c2-9730-1b73025ae0d1",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "e755fc2c-05c5-4459-b830-77533355711a",
+                    "date": "25-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                },
+                {
+                    "session_id": "e0a594b2-fe44-4755-ab21-c66b8fc5606e",
+                    "date": "27-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-06:00PM"
+                    ]
+                }
+            ]
+        },
+        {
+            "center_id": 651284,
+            "name": "DGD Nawada PHC 1",
+            "address": "Nawada Near MCD PRI School Nawada Near Nawada Metro Station , Delhi 110059",
+            "state_name": "Delhi",
+            "district_name": "South West Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 28,
+            "long": 77,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "5880a4af-b423-4777-a3ae-f3842893ee53",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                }
+            ]
+        },
+        {
+            "center_id": 651331,
+            "name": "MCW Uttam Nagar PHC 1",
+            "address": "MAHARAJA AGARSEN PLYCLINIC OLD UTTAM NAGAR",
+            "state_name": "Delhi",
+            "district_name": "South West Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 28,
+            "long": 77,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "797343a0-45d6-46f4-b07c-b1e905461808",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVAXIN",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                }
+            ]
+        },
+        {
+            "center_id": 651299,
+            "name": "CGHS Janakpuri I PHC 1",
+            "address": "CGHS Janakpuri, Behind Super Speciality Hospital Janakpuri",
+            "state_name": "Delhi",
+            "district_name": "South West Delhi",
+            "block_name": "Not Applicable",
+            "pincode": 110059,
+            "lat": 28,
+            "long": 77,
+            "from": "09:00:00",
+            "to": "17:00:00",
+            "fee_type": "Free",
+            "sessions": [
+                {
+                    "session_id": "4fb807fa-3691-45da-b948-b66af680596e",
+                    "date": "24-05-2021",
+                    "available_capacity": 0,
+                    "min_age_limit": 45,
+                    "vaccine": "COVISHIELD",
+                    "slots": [
+                        "09:00AM-11:00AM",
+                        "11:00AM-01:00PM",
+                        "01:00PM-03:00PM",
+                        "03:00PM-05:00PM"
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+
+
+
+
+var apiurl = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin'
+var data
+
+$('#BtnSubmit').click(
+    function () {
+        $('div[data-con-row-id]').remove()
+
+        var pincodeData = $('#inpPincode').val()
+        var fromDateData = $('#inpFromDate').val()
+
+        if (pincodeData == '') {
+            alert('Please provide Pincode')
+            return;
+        }
+
+        if (fromDateData == '') {
+            alert('Please provide FromDate')
+            return;
+        }
+
+        var year = fromDateData.split('-')[0]
+        var month = fromDateData.split('-')[1]
+        var day = fromDateData.split('-')[2]
+        var requestDate = day + '-' + month + '-' + year
+
+        console.log('pincodeData ' + pincodeData, 'fromDateData ' + fromDateData, 'requestDate ' + requestDate)
+
+        $.ajax(
+            {
+                url: apiurl,
+                method: 'GET',
+                success: function (data) {
+                    populateData(data)
+                },
+                data: {
+                    pincode: pincodeData,
+                    date: requestDate
+                },
+                error: function (XhR, Status, err) {
+                    // alert("Request failed " + Status)
+                    console.log(XhR.responseJSON)
+                    alert(XhR.responseJSON.error)
+                }
+                ,
+                async: false
+            }
+        )
+
+
+    }
+)
+
+// var sessionMap = new Map()
+// var vaccineMap = new Map()
+// var centerMap = new Map()
+
+function populateData(data) {
+    console.log(data)
+
+    /* remove this for original */
+    //data = tempData
+
+    if (!data) {
+        alert("No Vaccination Sessions available for this date.")
+        return;
+    }
+
+    if(data.centers.length == 0){
+        alert("No Vaccination Sessions available for this date.")
+        return;
+    }
+
+    for (var i = 0; i < data.centers.length; i++) {
+        var center = data.centers[i]
+        console.log(i , center)
+
+        $('<div>', {
+            'class': "row",
+            'data-con-row-id': `${i}`
+        }).appendTo('.container')
+
+        $('<div>', {
+            'class': "col-12"
+        }).appendTo('div[data-con-row-id="' + `${i}` +'"]')
+
+        $('<table>', {
+            "class": "table table-striped p-2 ",
+            "data-table-id": `${i}`
+        }).appendTo('div[data-con-row-id="'+ `${i}` + '"] .col-12')
+
+        $('<caption>', {
+            'html': '<b>Center ID:</b> ' + center.center_id + ' <b>Center Name:</b> ' + center.name,
+        }).appendTo('table[data-table-id="' + `${i}` + '"]')
+
+        $('<thead>').appendTo('table[data-table-id="' + `${i}` + '"]')
+
+        $('<tbody>').appendTo('table[data-table-id="' + `${i}` + '"]')
+
+        $(' <tr><th scope="col">#</th><th scope="col">Date</th><th scope="col">Available Capacity</th><th scope="col">Min Age Limit</th>' +
+            '<th scope="col">Vaccine</th>' +
+            '<th scope="col">Fee</th>' +
+            '<th scope="col">Slots</th>' +
+            '<th scope="col">From</th>' +
+            '<th scope="col">To</th></tr>').appendTo('table[data-table-id="' + `${i}` + '"] > thead')
+
+        var sessions = tempData.centers[i].sessions
+        // var vaccines = tempData.centers[i].vaccine_fees
+        
+        for (var j = 0; j < sessions.length; j++) {
+            var session = sessions[j]
+            console.log(j , session)
+            // for (var k = 0; k < vaccines.length; k++) {
+            //     var vaccine = vaccines[k].vaccine
+            //     var fee = vaccines[k].fee
+
+            //     if (vaccine == session.vaccine) {
+            //         $('<tr><td>' + (j + 1) + '</td><td>' + session.date + '</td><td>' + session.available_capacity + '</td><td>' + session.min_age_limit + '</td><td>' + session.vaccine + '</td><td>' + fee + '</td><td>' + session.slots.join(', ')
+            //             + '</td><td>' + center.from + '</td><td>' + center.to + '</td></tr>').appendTo('table[data-table-id="${i}"]')
+            //         break;
+            //     } else {
+            //         $('<tr><td>' + (j + 1) + '</td><td>' + session.date + '</td><td>' + session.available_capacity + '</td><td>' + session.min_age_limit + '</td><td>' + session.vaccine + '</td><td>' + 'Unavailable' + '</td><td>' + session.slots.join(', ')
+            //             + '</td><td>' + center.from + '</td><td>' + center.to + '</td></tr>').appendTo('table[data-table-id="${i}"]')
+            //         break;
+            //     }
+            // }
+            
+            $('<tr><td>' + (j + 1) + '</td><td>' + session.date + '</td><td>' + session.available_capacity + '</td><td>' + session.min_age_limit + '</td><td>' + session.vaccine + '</td><td>' + center.fee_type + '</td><td>' + session.slots.join(', ')
+            + '</td><td>' + center.from + '</td><td>' + center.to + '</td></tr>').appendTo('table[data-table-id="'+ `${i}` + '"]')
+        
+        }
+    }
+
+}
+
+
+
